9b2db28d039b02ff90378ec82907fcd4da63a548
RyoScorpio <belandresjozell63@gmail.com>
2025-01-13 21:45:42 +0800

    potential version of ComProg Prac BSCS 1-1.py
    Changelog:
    1. Added while loops under each menu option. This gives the user the option to continue with their last action seamlessly.
    2. Added a lot of comments for new and old code
    3. Fixed unnecessary lines of code

--- ComProg Prac BSCS 1-1.py
+++ ComProg Prac BSCS 1-1.py
@@ -10,8 +10,8 @@ alphabetical" and "by price".
 Yun lang yung kulang as far as I know. Paki-triple check nalang yung code ko thank you!!!
 """
 
-
-
+priceIndex = 1
+prodPrice = 1
 shop = []
 price = []
 
@@ -22,35 +22,57 @@ while True: # This loop ensures that the program is always running until it's pr
     userInput = input("Option: ")
     menuInput = userInput.upper() # Converts the input into uppercase
     if menuInput == "A": # User adds a product on the list
-        prodName = input("Product: ") # Prompts the user to add a product
-        prodName = prodName.upper() # Converts the input to uppercase
-        if prodName in shop: # Stops the user from adding the same product
-            print("*This product already exists.*")
-            print()
-        else:
-            shop.append(prodName) # Adds the inputted product to the list
         while True:
-            try:
-                price.append(float(input("Price (PhP): "))) # Prompts the user to assign a price
-                print("*Successfully added the product.*")
+            if prodPrice == 0: # Stops [A]dd command when cancelled from the price input
+                prodPrice = 1
+                break
+            print()
+            print("(Enter [E] to cancel.)")
+            prodName = input("Product: ") # Prompts the user to add a product
+            prodName = prodName.upper() # Converts the input to uppercase
+            if prodName in shop: # Stops the user from adding the same product
+                print("*This product already exists.*")
+                print()
+            elif prodName == "E": # Stops [A]dd command
+                print("*Operation cancelled.*")
                 print()
                 break
-            except ValueError: # Prints an error if the inputted price is NOT a number
-                print("*Invalid input. Please enter a number.*")
-                print()
-                continue
+            else:
+                shop.append(prodName) # Adds the inputted product to the list
+            while True:
+                try:
+                    print("(Enter [0] to cancel.)")
+                    prodPrice = float(input("Price (PhP): ")) # Prompts user to input the price
+                    if prodPrice == 0: # Stops [A]dd command
+                        shop.remove(prodName)
+                        print("*Operation cancelled.*")
+                        print()
+                        break
+                    else:
+                        price.append(prodPrice) # Prompts the user to assign a price
+                        print("*Successfully added the product.*")
+                        break
+                except ValueError: # Prints an error if the inputted price is NOT a number
+                    print("*Invalid input. Please enter a number.*")
+                    print()
     elif menuInput == "R": # User removes a product from the list
-        prodName = input("Product: ")
-        prodName = prodName.upper()
-        if prodName in shop: # Checks if the product exists in the product list
-            priceIndex = shop.index(prodName) # Matches the product with its corresponding price
-            price.pop(priceIndex) # Removes the price from the price list
-            shop.remove(prodName) # Removes the product from the price list
-            print("*Product removed successfully.*")
-            print()
-        else: # Executes if the input is invalid
-            print("*The product does not exist.*")
-            print()
+        while True:
+            print("(Enter [E] to cancel.)")
+            prodName = input("Product: ")
+            prodName = prodName.upper()
+            if prodName in shop: # Checks if the product exists in the product list
+                priceIndex = shop.index(prodName) # Matches the product with its corresponding price
+                price.pop(priceIndex) # Removes the price from the price list
+                shop.remove(prodName) # Removes the product from the price list
+                print("*Product removed successfully.*")
+                print()
+            elif prodName == "E": # Stops [R] command
+                print("*Operation cancelled.*")
+                print()
+                break
+            else: # Executes if the input is invalid
+                print("*The product does not exist.*")
+                print()
     elif menuInput == "L": # User views the list of available products
         print()
         print("{:25} {}".format("Product", "Price")) # Prints the labels for both lists
@@ -59,24 +81,41 @@ while True: # This loop ensures that the program is always running until it's pr
         print(itemList)
         print()
     elif menuInput == "U": # User updates the price of an existing product
-        prodName = input("Product: ")
-        prodName = prodName.upper()
-        if prodName in shop: # Checks if the product exists in the product list
-            priceIndex = shop.index(prodName) # Matches the product with its corresponding price
-            while True:
-                try:
-                    price[priceIndex] = float(input("Price: ")) # Prompts the user to change the selected product's price
+        while True:
+            # if prodPrice == 0: # Stops [U]pdate command if cancelled from price input
+                # prodPrice = 1
+                # break
+            print()
+            print("(Enter [E] to cancel.)")
+            prodName = input("Product: ")
+            prodName = prodName.upper()
+            if prodName in shop: # Checks if the product exists in the product list
+                priceIndex = shop.index(prodName) # Matches the product with its corresponding price
+                break
+            elif prodName == "E": # Stops [U]pdate command
+                print("*Operation cancelled.*")
+                print()
+                break
+            else:  # Executes if the input is invalid
+                print("*The product does not exist.*")
+        while True:
+            try:
+                print("(Enter [0] to cancel.)")
+                prodPrice = float(input("Price: ")) # Prompts the user to change the selected product's price
+                if prodPrice == 0: # Stops [U]pdate command
+                    print("*Operation cancelled.*")
+                    print()
+                    break
+                else:
+                    price[priceIndex] = prodPrice # Changes the selected product price
                     print("*Price successfully changed.*")
                     print()
                     break
-                except ValueError: # Prints an error if the inputted price is NOT a number
-                    print("*Invalid input. Please enter a number.*")
-                    print()
-                    continue
-        else: # Executes if the input is invalid
-            print("*The product does not exist.*")
-            print()
+            except ValueError: # Prints an error if the inputted price is NOT a number
+                print("*Invalid input. Please enter a number.*")
+                print()
     elif menuInput == "E": # Stops the program
+        print("*Program successfully stopped.*")
         break
     else: # Prints an error if the user selects an invalid menu option
         print("*Invalid input.*")
